9e84f5f7b4c7763088ee12286010d1c622919046
kimgip <gippeum0102@gmail.com>
2024-07-25 09:32:03 +0900

    webpack config: module: rules

--- ex04/webpack.config.js
+++ ex04/webpack.config.js
@@ -7,11 +7,17 @@ module.exports = {
         path: path.resolve('public'),
         filename: 'assets/js/main.js'
     },
+    module: {
+        rules:[{
+            test: /\.css$/i,
+            use:['style-loader', 'css-loader']
+        }]
+    },
     devServer: {
         host: '0.0.0.0',
         port: 9090,
         liveReload: true,
         compress: true,
         hot: false
-    }        
+    }
 }
--- ex05/webpack.config.js
+++ ex05/webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
         path: path.resolve('public'),
         filename: 'assets/js/main.js'
     },
+    module: {
+        rules:[{
+          test: /\.(c|sa|sc)ss$/i,
+          use:['style-loader', 'css-loader', 'sass-loader']
+        }]
+      },
     devServer: {
         host: '0.0.0.0',
         port: 9090,
